1c3abdb57bfb0cde489d567cef1406cb7cee2cc7
oNgStIng <ongsting@oNgStIngdeMacBook-Air.local>
2015-12-31 00:44:57 +0800

    The icon will be shown only when the host is "zhihu"

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
         // That fires when a page's URL contains a 'g' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'g' },
+            pageUrl: { hostContains: 'zhihu' },
           })
         ],
         // And shows the extension's page action.
