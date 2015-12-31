164afd4977c7b86901e2b7d0727ffdc4eae47088
oNgStIng <ongsting@oNgStIngdeMacBook-Air.local>
2015-12-31 21:23:16 +0800

    过滤掉蠢人的回答 显示有多少愚蠢的回答被屏蔽

--- filter.js
+++ filter.js
@@ -1,5 +1,40 @@
+// stupid
+var stupid = ["陈玲玲", "ZJ Ye", "龙少", "彼岸小棠", "大海", "望息", "黄轶轩", "蛟羽", "behow", "罗小鼎", "赵一凡", "嘎巴贴", "陈虹", "米兰鸭子", "李尚泽", "王义德", "離娮", "马鑫", "侯铁", "doriko", "范遥", "邢汉语", "煙花释", "Lyn Misery", "ZJ Ye", "沈少Neo", "楚莫敖", "君莫笑", "不想说不知道", "冯路", "Sunny", "宫本刀", "李鼎", "庸人·沈在河", "木吉啦"];
+
+var stupid_answer_number = 0;
+
+// filters
+
+function modify_answer_num() {
+  var answer_node = document.getElementById("zh-question-answer-num");
+  var answer_num = answer_node.getAttribute("data-num");
+  answer_node.innerHTML = (answer_num - stupid_answer_number) + " 个回答（" + stupid_answer_number + " 个蠢人被屏蔽）";
+}
+
+function filter_answer(ele) {
+  var who = ele.children[3].getAttribute("data-author-name");
+  if (stupid.find((x) => x == who) && ele.parentNode){
+    ele.parentNode.removeChild(ele);
+    stupid_answer_number ++;
+    return true;
+  }
+  return false;
+}
+
+// at the first load
+
+var elements = document.getElementsByClassName("zm-item-answer");
+for (var i = 0; i < elements.length; i++) {
+  var ele = elements[i];
+  if (filter_answer(ele))
+    i--;
+}
+modify_answer_num();
+
+// add listener to the DOM change
+
 var observer = new MutationSummary({
-  callback: filter,
+  callback: filter_handler,
   rootNode: document,
   observeOwnChanges: false,
   queries: [{
@@ -7,16 +42,9 @@ var observer = new MutationSummary({
   }]
 });
 
-function filter(summaries) {
+function filter_handler(summaries) {
   var summary = summaries[0];
-
-  summary.added.forEach(function(ele){
-    if (ele.parentNode)
-      ele.parentNode.removeChild(ele);
-  });
-
+  stupid_answer_number = 0;
+  summary.added.forEach(filter_answer(ele));
+  modify_answer_num();
 }
-
-
-var element = document.getElementById("zh-question-answer-wrap");
-element.innerHTML = "";
