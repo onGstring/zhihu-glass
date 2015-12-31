5c92cb3ecf6e3c314de01902f52c4a849bc23719
oNgStIng <ongsting@oNgStIngdeMacBook-Air.local>
2015-12-31 16:29:26 +0800

    it removes all answers

--- filter.js
+++ filter.js
@@ -1,2 +1,22 @@
+var observer = new MutationSummary({
+  callback: filter,
+  rootNode: document,
+  observeOwnChanges: false,
+  queries: [{
+    element: "div.zm-item-answer"
+  }]
+});
+
+function filter(summaries) {
+  var summary = summaries[0];
+
+  summary.added.forEach(function(ele){
+    if (ele.parentNode)
+      ele.parentNode.removeChild(ele);
+  });
+
+}
+
+
 var element = document.getElementById("zh-question-answer-wrap");
 element.innerHTML = "";
